bbb3b3be63bb2330ebb6505da8cb93e109d61e68
hussien shokry <hessinshokry3@gmail.com>
2024-10-26 02:32:13 +0300

    edit anchor links in scripts for better smooth pop up model

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,17 @@ document.addEventListener('DOMContentLoaded', function () {
     // Smooth scrolling for anchor links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
+            const targetId = this.getAttribute('href');
+            // Proceed only if targetId is not just '#'
+            if (targetId !== '#') {
+                e.preventDefault();
+                const targetElement = document.querySelector(targetId);
+                if (targetElement) {
+                    targetElement.scrollIntoView({
+                        behavior: 'smooth'
+                    });
+                }
+            }
         });
     });
 
